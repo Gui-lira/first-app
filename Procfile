a3785422aa6ef177fa41871b7876439f1e42b707
Gui-lira <gui.lira.martins@gmail.com>
2021-10-17 20:41:30 -0300

    correções nas configurações

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn first-app.wsgi
+web: gunicorn firstOne.wsgi
--- firstOne/settings.py
+++ firstOne/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 import django_heroku
 from pathlib import Path
 
@@ -21,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jezblr$6l$7ni2i-k#g@=-xum)o13xwj382i&nbxw0)t&+o0m+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 
 ALLOWED_HOSTS = ['https://primeirodjango.herokuapp.com/']
 
